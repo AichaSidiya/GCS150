4a401ad83855ab3e0e744d0d0237eb1485d150d1
AichaSidiya <91727165+AichaSidiya@users.noreply.github.com>
2023-02-18 17:04:42 +0300

    Add files via upload

---
+++ FileExercices/read.py
@@ -0,0 +1,6 @@
+def main():
+    infile=open('philosopher.txt','r')
+    content=infile.read()
+    infile.close()
+    print(content)
+main()
---
+++ FileExercices/sequential.py
@@ -0,0 +1,11 @@
+def main():
+    infile=open('philosopher.txt','r')
+    line1=infile.readline()
+    line2=infile.readline()
+    line3=infile.readline()
+    infile.close()
+    print(line1)
+    print(line2)
+    print(line3)
+
+main()
