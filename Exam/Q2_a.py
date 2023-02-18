a2bd6d000c8391d28bb8cebdcb734b70f39645e5
AichaSidiya <91727165+AichaSidiya@users.noreply.github.com>
2023-02-18 18:01:29 +0300

    Update and rename Q2_a.py to updatingVariables.py

--- Exam/Q2_a.py
+++
@@ -1,7 +0,0 @@
-x=int(input(‘Enter x : ‘)
-y=0
-i=x
-while i>1 :
-    y=y+i
-    i-=2
-print(‘y is :’y)
---
+++ Exam/updatingVariables.py
@@ -0,0 +1,7 @@
+x=int(input('Enter x : ')
+y=0
+i=x
+while i>1 :
+    y=y+i
+    i-=2
+print('y is :', y)
